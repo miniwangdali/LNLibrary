34c975147d9472bd0fe66b323725742ab908b97c
miniwangdali <wang__dali@sina.com>
2015-08-30 07:24:24 +0800

    删除无用文件

--- src/com/victor/lnlibrary/book/XmlParser.java
+++
@@ -1,9 +0,0 @@
-package com.victor.lnlibrary.book;
-
-import java.io.InputStream;
-
-public interface XmlParser{
-	public Book parse(InputStream is) throws Exception;
-
-	public String serialize(Book book) throws Exception;
-}
